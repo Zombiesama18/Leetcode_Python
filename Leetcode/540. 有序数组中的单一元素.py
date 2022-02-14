eb1ebbbcfe24fb98a772eb02879877bbec45421e
zombiesama18 <435719390@qq.com>
2022-02-15 00:25:06 +0800

    Changes 20220215

--- Leetcode/540. 有序数组中的单一元素.py
+++ Leetcode/540. 有序数组中的单一元素.py
@@ -21,3 +21,25 @@ def singleNonDuplicate(nums: List[int]) -> int:
             counter += 1
     return lastNumber
 
+
+def singleNonDuplicateVersion2(nums: List[int]) -> int:
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if mid % 2 == 0:
+            if nums[mid] == nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        else:
+            if nums[mid] == nums[mid - 1]:
+                left = mid + 1
+            else:
+                right = mid
+    return nums[left]
+
+
+singleNonDuplicateVersion2([1,1,2,3,3,4,4,8,8])
+singleNonDuplicateVersion2([3,3,7,7,10,11,11])
+
+
